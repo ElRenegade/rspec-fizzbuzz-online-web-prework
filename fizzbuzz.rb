aac2ff38ffd16003db709271001ab27b6419e025
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-09 05:28:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
     answer = "FizzBuzz"
-  end
+
+elsif num % 3 == 0
+  answer2 = "Fizz"
+end
 # if num % 3 == 0
 #     answer2 = "Fizz"
 #   elsif num % 5 == 0
